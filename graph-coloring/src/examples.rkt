d54e4ebbfb63f7c801cb01a9e0b8a76291e22227
slxu <slxu@cs.washington.edu>
2014-10-24 16:28:52 -0700

    my solution to hw1

--- graph-coloring/src/examples.rkt
+++ graph-coloring/src/examples.rkt
@@ -5,10 +5,28 @@
 ; Extract all small easy problems (see problems.rkt):
 (define small-problems
   (filter (lambda (p) 
-          (eq? (problem-difficulty p) 'easy)
-          (< (problem-nodes p) 30)
-          (< (problem-edges p) 30))
-        problems))
+            (eq? (problem-difficulty p) 'easy)
+            (< (problem-nodes p) 30)
+            (< (problem-edges p) 30))
+          problems))
+
+(define easy-problems
+  (filter (lambda (p) (eq? (problem-difficulty p) 'easy)
+            )
+          problems))
+
+(define medium-problems
+  (filter (lambda (p) 
+            (eq? (problem-difficulty p) 'medium)
+            )
+          problems))
+
+(define hard-problems
+  (filter (lambda (p) 
+            (eq? (problem-difficulty p) 'hard)
+            )
+          problems))
+
 
 ; Print them (there is just one):
 small-problems
@@ -18,14 +36,12 @@ small-problems
   (problem->graph (first small-problems)))
 
 ; The following vector represents a valid coloring for small-graph (see k-coloring.rkt):
-(define small-graph-coloring #(0 1 3 1 0 0 1 3 3 0 2))
+;(define small-graph-coloring #(0 1 3 1 0 0 1 3 3 0 2))
 
-(valid-coloring? small-graph small-graph-coloring)
+;(valid-coloring? small-graph small-graph-coloring)
 
 ; If you have dot installed on your system, uncomment the following 
 ; lines to visualize small-graph:
-;(dot "/opt/local/bin/dot")
-;(visualize small-graph small-graph-coloring)
 
 ; Runs your k-coloring procedure (see k-coloring.rkt) on the provided problem, 
 ; printing timing data and #t/#f depending on whether the produced 
@@ -37,12 +53,27 @@ small-problems
   (define coloring (time (k-coloring graph k)))
   (printf "valid-coloring? ~a\n" (valid-coloring? graph coloring)))
 
-; Uncomment to test your encoding of small-problem:
-; (run small-problem)
 
+
+(dot "/opt/local/bin/dot")
+
+; Uncomment to test your encoding of small-problem:
+(run ( first small-problems))
+(visualize small-graph (k-coloring small-graph (problem-colors ( first small-problems))))
+;(print small-graph)
+;(print ( single-edge-encoding 2 3 4))
+;(adjacent-vertices-have-different-color-encoding small-graph (problem-colors ( first small-problems)))
+;(k-coloring-encoding small-graph (problem-colors ( first small-problems)))
+;(vertex-color-to-variable 0 1 4)
+;(variable-to-vertex 2 4)
+;(variable-to-color 2 4)
+;(k-coloring small-graph (problem-colors ( first small-problems)))
 ; A SAT solver accepts a CNF formula, represented as a list of lists of 
 ; integers, and returns #f or an interpretation, depending on whether the 
 ; formula is satisfiable or not (see solver.rkt).
-(solve '((1 -2) (-1) (2))) ; unsat
-(solve '((1 -2) (1)))      ; sat
+;(solve '((1 -2) (-1) (2))) ; unsat
+;(solve '((1 -2) (1)))      ; sat
 
+(for ([ p hard-problems])
+     (   run p    )
+     )
--- graph-coloring/src/k-coloring.rkt
+++ graph-coloring/src/k-coloring.rkt
@@ -3,18 +3,96 @@
 (require "solver.rkt" "graph.rkt")
 
 (provide
- k-coloring      ; (->* (graph/c natural-number/c) [(is-a? solver%)] (or/c #f coloring/c))
- valid-coloring? ; (-> graph/c coloring/c boolean?)
-)
+  k-coloring      ; (->* (graph/c natural-number/c) [(is-a? solver%)] (or/c #f coloring/c))
+  valid-coloring? ; (-> graph/c coloring/c boolean?)
+  )
 
 ; Returns true iff the given coloring is correct for the specified graph.
 (define (valid-coloring? graph coloring)
   (for*/and ([(e n) (in-indexed graph)] [child e])
-     (not (= (color-ref coloring n) (color-ref coloring child)))))
+            (not (= (color-ref coloring n) (color-ref coloring child)))))
+
+; use vertex_index*k+1, ..., vertex_index*k+c .. vertex_index*k+k  to denote
+; that vertex of vertex_index has color c
+(define (vertex-color-to-variable v c k)
+  (+ (+ (* v  k)  c ) 1)
+  )
+
+(define (variable-to-vertex var k)
+  (quotient (- var  1)  k)
+  )
+
+(define (variable-to-color var k)
+  (- (- var  1 )  (* k  ( variable-to-vertex var k)))
+  )
+
+(define  (sesc v w c k) (list (- (vertex-color-to-variable v c k)) (- (vertex-color-to-variable w c k))))
+
+(define (single-edge-single-color-encoding v w c k)
+  (if (= (+ c 1) k)
+    (list (sesc v w c k))
+    (cons (sesc v w c k) (single-edge-single-color-encoding v w (+ c  1) k))
+    )
+  )
+
+(define (single-edge-encoding v w k)
+  (single-edge-single-color-encoding v w 0 k)
+  )
+
+(define (adjacent-vertices-have-different-color-encoding graph k)
+  (foldl (lambda (vertex-idx le) 
+           ( append  le (foldl (lambda (child le) 
+                                 (append le (single-edge-encoding vertex-idx child k))
+                                 )
+                               '()
+                               (sequence-ref graph vertex-idx))
+                     )
+           )
+         '() 
+         (build-list (sequence-length graph) values)
+         )
+  )
+
+(define (vertex-has-one-color-encoding graph k)
+  (map 
+    (lambda (vertex-idx)
+      (foldl (lambda (c l)
+               (append l (list ( vertex-color-to-variable vertex-idx c k)))
+               )  '()  (build-list k values)
+             )
+      )
+    (build-list (sequence-length graph) values)
+    )
+  )
+
+(define (k-coloring-encoding graph k)
+  (append (vertex-has-one-color-encoding graph k) 
+          (adjacent-vertices-have-different-color-encoding graph k) )
+  )
+
+
+(define (get-colorings solve-result k)
+  (define positive-ones (filter positive? solve-result))
+  (list->vector (map (lambda (x) 
+                       (variable-to-color x k ))
+                     (sort (filter (lambda (e) (
+                                                for/and ([ c (in-range (variable-to-color e k))]) (not (member (vertex-color-to-variable (variable-to-vertex e k) c k) positive-ones))
+                                                ))
+                                   positive-ones
+                                   ) <)
+                     ))
+  )
+
 
 ; Returns a coloring/c if the given graph can 
 ; be colored with k colors.  Otherwise returns #f.
 ; The procedure accepts an optional solver% arugment.
 ; If not specified, the solver defaults to lingeling.
 (define (k-coloring graph k [solver (lingeling)])
-  (error 'k-coloring "not implemented yet!"))
+  ; encode each vertex must have a coloring
+  (define do-solve (solve (k-coloring-encoding graph k)))
+  (if do-solve
+    (get-colorings do-solve k) ; k-colorable
+    #f
+    )
+  )
