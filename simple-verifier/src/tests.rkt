5bce12cd7ae2039dbb2d31be7b50caba377b9dd2
slxu <slxu@cs.washington.edu>
2014-10-30 17:57:55 -0700

    working

--- simple-verifier/src/tests.rkt
+++ simple-verifier/src/tests.rkt
@@ -6,10 +6,10 @@
 ; Verify all benchmarks:
 (define (verify-all)
  (run-verifier max1 max2)
- ;(run-verifier max1 max3)
- ;(run-verifier floor-of-ave1 floor-of-ave2)
- ;(run-verifier flip-right-01 flip-right-02)
- ;(run-verifier isolate-right-01 isolate-right-02)
+ (run-verifier max1 max3)
+ (run-verifier floor-of-ave1 floor-of-ave2)
+ (run-verifier flip-right-01 flip-right-02)
+ (run-verifier isolate-right-01 isolate-right-02)
  )
 
 (define (run-verifier f1 f2)
--- simple-verifier/src/verifier.rkt
+++ simple-verifier/src/verifier.rkt
@@ -19,8 +19,16 @@
 ; (-> fragment? fragment? (or/c 'EQUIVALENT (listof integer?))))
 (define (verify f1 f2)
   (define s (generate-z3-input f1 f2))
-  (printf (~a s))
+  ;(printf (~a s))
   (solve s))
+
+(define expression-type-hash
+  (make-hash '((= . Bool) (bvule . Bool) (bvult . Bool) (bvuge . Bool) (bvugt . Bool) 
+               (bvsle . Bool) (bvslt . Bool) (bvsge . Bool) (bvsgt . Bool)
+               (bvneg . (_ BitVec 32)) (bvadd . (_ BitVec 32)) (bvmul . (_ BitVec 32))
+               (bvnot . (_ BitVec 32)) (bvdiv . (_ BitVec 32)) (bvsrem . (_ BitVec 32))
+               (bvshl . (_ BitVec 32)) (bvlshr . (_ BitVec 32)) (bvashr . (_ BitVec 32))
+               (bvsub . (_ BitVec 32)) (bvor . (_ BitVec 32)) (bvand . (_ BitVec 32)) (bvxor . (_ BitVec 32)))))
 
 (define (generate-z3-input f1 f2)
   (define f1parameters (extract-parameters f1))
@@ -43,59 +51,95 @@
 
 (define (declare-functions frgmt parameters prefix)
   (define statements (extract-statements frgmt))
-  (define parameters-hash (make-hash (map (lambda (b) (list b (string->symbol (string-append prefix (~a b) )))) parameters)))
+  (define parameters-hash (make-hash (map (lambda (b) (cons b (string->symbol (string-append prefix (~a b) )))) parameters)))
   (define variable-fun-name-hash (make-hash))
+  (define fun-type-hash (make-hash))
   (map (lambda (b) 
          (let* [(terms (syntax-e b))
                 (head (syntax-e (car terms)))
                 (body (cdr terms))]
          (cond 
-           [(eq? head 'define) (process-define-statement parameters prefix body parameters-hash variable-fun-name-hash)]
-           [(eq? head 'return) (process-return-statement parameters prefix body parameters-hash variable-fun-name-hash)]
-           [(eq? head 'set!) (process-set-statement parameters prefix body parameters-hash variable-fun-name-hash)]
+           [(eq? head 'define) (process-define-statement parameters prefix body parameters-hash variable-fun-name-hash fun-type-hash)]
+           [(eq? head 'return) (process-return-statement parameters prefix body parameters-hash variable-fun-name-hash fun-type-hash)]
+           [(eq? head 'set!) (process-set-statement parameters prefix body parameters-hash variable-fun-name-hash fun-type-hash)]
            [else (error (string-append "unknown statement head:" (~a head)))]
            )))
        statements)
   )
 
-(define (declare-expression parameters prefix stmt parameters-hash variable-fun-name-hash) 
+;return: (type, list of symbols )
+(define (declare-expression parameters prefix stmt parameters-hash variable-fun-name-hash fun-type-hash) 
   (begin
   (define stmt-content (syntax-e stmt))
+  ;(printf (string-append "currently expression: " (~a stmt-content)))
   (cond  
-    [(symbol? stmt-content) (cond [(eq? stmt-content 'true) 'true ]
-                          [(eq? stmt-content 'false) 'false ]
-                          [else (car (hash-ref parameters-hash stmt-content (list (cons (hash-ref variable-fun-name-hash (~a stmt-content) "") 
-                                                                                                     (prefix-parameters parameters prefix))) ))])] ; id or true or false
-    [(number? stmt-content) (list '_ (string->symbol (string-append "bv" (~a stmt-content))) '32)] ; const
-    [else (let [(head (syntax->datum (car stmt-content)))]
-            (cons (if (eq? head 'if ) 'ite  head )  (map (lambda (b) (declare-expression parameters prefix b parameters-hash variable-fun-name-hash)) (cdr stmt-content))))]
+    [(symbol? stmt-content) (cond [(eq? stmt-content 'true) (list 'Bool 'true) ]
+                          [(eq? stmt-content 'false) (list 'Bool 'false) ]
+                          [else (let* [(parameter-name (hash-ref parameters-hash stmt-content ""))
+                                      (function-name (hash-ref variable-fun-name-hash (~a stmt-content) ""))
+                                      (function-call-signature (cons function-name (prefix-parameters parameters prefix)))]
+                                    (begin ;(printf (string-append "parameter-name: " (~a parameter-name)))
+                                           ;(printf (string-append "function-name: " (~a function-name)))
+                                           ;(printf (string-append "function-call-signature: " (~a function-call-signature)))
+                                         (if (symbol? parameter-name)
+                                    (list '(_ BitVec 32) parameter-name)
+                                    (begin ;(printf (string-append "fun-name: " (~a function-name) " fun-type: " (~a (hash-ref fun-type-hash function-name "")) "\n"))
+                                           ;(printf (string-append "fun-type-hash: " (~a fun-type-hash) "\n"))
+                                           (list (hash-ref fun-type-hash function-name "") function-call-signature)))
+                                         ))
+                                ;(car (hash-ref parameters-hash stmt-content function-call))
+                                ])] ; id or true or false
+    [(number? stmt-content) (if (< stmt-content 0)
+                              (list '(_ BitVec 32) (list 'bvneg (list '_ (string->symbol (string-append "bv" (~a (- stmt-content)))) '32))); <0
+                              (list '(_ BitVec 32) (list '_ (string->symbol (string-append "bv" (~a stmt-content))) '32)); >=0
+                             )] ; const
+    [else (let* [(head (syntax->datum (car stmt-content)))
+                 (sub-expressions (map (lambda (b) 
+                                         (begin 
+                                           ;(printf (string-append "exp: " (~a (syntax->datum b)) " we get: " (~a (declare-expression parameters prefix b parameters-hash variable-fun-name-hash fun-type-hash)) "\n"))
+                                                (declare-expression parameters prefix b parameters-hash variable-fun-name-hash fun-type-hash))) (cdr stmt-content)))]
+          (if (eq? head 'if )
+            (begin ;(printf (string-append "if subexp: " (~a sub-expressions) "\n"))
+                   (list (car (car (cdr sub-expressions))) (cons 'ite (sub-expressions-to-symbol-list sub-expressions)))) ; if expression
+             (begin 
+                ;(printf (string-append "sub-expressions: " (~a sub-expressions)))
+ (list (hash-ref expression-type-hash head) (cons head (sub-expressions-to-symbol-list sub-expressions)))); others
+            ))]
   )))
+
+(define (sub-expressions-to-symbol-list sub-expressions)
+  (map (lambda(a)(car (cdr a))) sub-expressions))
 
 ;list of new symbols
 (define (prefix-parameters parameters prefix)
   (map (lambda (b) (string->symbol (string-append prefix (~a b) )) ) parameters))
 
-(define (process-return-statement parameters prefix body parameters-hash variable-fun-name-hash)
+(define (process-return-statement parameters prefix body parameters-hash variable-fun-name-hash fun-type-hash)
   (define fun-name (string->symbol (string-append prefix "return")))
-  (list 'define-fun fun-name (parameters-as-fun-parameters parameters prefix) '(_ BitVec 32)
-    (declare-expression parameters prefix (car body) parameters-hash variable-fun-name-hash)))
+  (define expression-declared (declare-expression parameters prefix (car body) parameters-hash variable-fun-name-hash fun-type-hash))
+  (list 'define-fun fun-name (parameters-as-fun-parameters parameters prefix) (car expression-declared)
+    (car (cdr expression-declared))))
 
-(define (process-define-statement parameters prefix body parameters-hash variable-fun-name-hash)
+(define (process-define-statement parameters prefix body parameters-hash variable-fun-name-hash fun-type-hash)
   (define variable-name (~a (syntax-e (car body))))
   (define fun-name (string->symbol (string-append prefix variable-name)))
   (begin
     (hash-set! variable-fun-name-hash variable-name fun-name)
-    (list 'define-fun fun-name (parameters-as-fun-parameters parameters prefix) '(_ BitVec 32)
-      (declare-expression parameters prefix (car (cdr body)) parameters-hash variable-fun-name-hash) )))
+    (define expression-declared (declare-expression parameters prefix (car (cdr body)) parameters-hash variable-fun-name-hash fun-type-hash))
+    (hash-set! fun-type-hash fun-name (car expression-declared))
+    (list 'define-fun fun-name (parameters-as-fun-parameters parameters prefix) (car expression-declared)
+      (car (cdr expression-declared)))))
 
-(define (process-set-statement parameters prefix body parameters-hash variable-fun-name-hash)
+(define (process-set-statement parameters prefix body parameters-hash variable-fun-name-hash fun-type-hash)
   (define variable-name (~a (syntax-e (car body))))
   (define variable-old-fun-name (hash-ref variable-fun-name-hash variable-name))
   (define variable-new-fun-name (string->symbol (string-append prefix (symbol->string variable-old-fun-name))))
   (begin
     (hash-set! variable-fun-name-hash variable-name variable-new-fun-name)
-    (list 'define-fun variable-new-fun-name (parameters-as-fun-parameters parameters prefix) '(_ BitVec 32)
-      (declare-expression parameters prefix (car (cdr body)) parameters-hash variable-fun-name-hash) )))
+    (define expression-declared (declare-expression parameters prefix (car (cdr body)) parameters-hash variable-fun-name-hash fun-type-hash))
+    (hash-set! fun-type-hash variable-new-fun-name (car expression-declared))
+    (list 'define-fun variable-new-fun-name (parameters-as-fun-parameters parameters prefix) (car expression-declared)
+      (car (cdr expression-declared)))))
 
 (define (parameters-as-fun-parameters parameters prefix)
    (map (lambda (b) (list b '(_ BitVec 32))) (prefix-parameters parameters prefix))
